27c1affc4e192b99388918ea312f5282762b576f
ana-btrz <anabeatrizaraujolopes2004@gmail.com>
2025-05-16 22:59:09 -0300

    novos comandos feitos

--- xadrez.c
+++ xadrez.c
@@ -29,5 +29,23 @@ int main() {
     } while (movimentoRainha < casasRainha);
     printf("\n");
 
+    // Movimento do Cavalo (duas casas para baixo e uma para a esquerda) - usando 'for' e 'while' 
+    printf("Movimento do Cavalo:\n");
+    int movimentoCavaloTotal = 3; // O Cavalo se move 3 "casas" no total (2 + 1)
+    int passosCavalo = 0; // variével passosCavalo
+
+    // Primeiro, move duas casas para baixo (usando um loop for - controla o número de passos nessa direção)
+    for (int i = 0; i < 2; i++) {
+        printf("Baixo\n");
+        passosCavalo++;
+    }
+
+    // Depois, move uma casa para a esquerda (usando um loop while para controlar esse passo final)
+    while (passosCavalo < movimentoCavaloTotal) {
+        printf("Esquerda\n");
+        passosCavalo++;
+    }
+    printf("\n");
+    
     return 0;
 }
